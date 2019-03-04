e4eb29ac8ac209b33f531a47bb2fa499ee98b804
Dominik Michalik <dominik.michalik@vml.com>
2019-03-04 16:45:38 +0100

    Start with nav

---
+++ app/src/components/Nav.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {FaUsers} from 'react-icons/fa';
+
+class Nav extends Component {
+    render(){
+        return (
+
+        )
+    }
+}
+
+export default Nav;

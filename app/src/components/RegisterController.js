dd3f1195e8909da324a4426df3df995b364626a6
Dominik Michalik <dominik.michalik@vml.com>
2019-03-05 10:59:19 +0100

    Add bootstrap register form and catch data

--- app/src/components/RegisterController.js
+++ app/src/components/RegisterController.js
@@ -3,16 +3,110 @@
 import React, { Component } from 'react';
 
 class RegisterController extends Component{
+
+    // create constructor for empty objects, which waits for value
+    constructor(){
+        super();
+        this.state = {
+            displayName : '',
+            email: '',
+            passOne: '',
+            passTwo: ''
+        };
+
+        this.changeFieldValue = this.changeFieldValue.bind(this);
+    }
+
+    // set new state
+    changeFieldValue(event){
+        const name = event.target.name;
+        const value = event.target.value;
+
+        // refer to constructor to modify original object
+        this.setState({[name]: value});
+    }
+
     render(){
-
-        // const { user } = this.props;
-
         return (
-            <div>
+            <form className="mt-3">
                 <div className="container">
-                    <h1 className="text-primary text-center">Zarejestruj się</h1>
+                    <div className="row justify-content-center">
+                        <div className="col-lg-8">
+                            <div className="card bg-light">
+                                <div className="card-body">
+                                    <h3 className="font-weight-light mb-3">Register</h3>
+                                    <div className="form-row">
+                                        <section className="col-sm-12 form-group">
+                                            <label
+                                                className="form-control-label sr-only"
+                                                htmlFor="displayName"
+                                            >
+                                                Display Name
+                                            </label>
+                                            <input
+                                                className="form-control"
+                                                type="text"
+                                                id="displayName"
+                                                placeholder="Display Name"
+                                                name="displayName"
+                                                required
+                                                value={this.state.displayName}
+                                                onChange={this.changeFieldValue}
+                                            />
+                                        </section>
+                                    </div>
+                                    <section className="form-group">
+                                        <label
+                                            className="form-control-label sr-only"
+                                            htmlFor="email"
+                                        >
+                                            Email
+                                        </label>
+                                        <input
+                                            className="form-control"
+                                            type="email"
+                                            id="email"
+                                            placeholder="Email Address"
+                                            required
+                                            name="email"
+                                            value={this.state.email}
+                                            onChange={this.changeFieldValue}
+                                        />
+                                    </section>
+                                    <div className="form-row">
+                                        <section className="col-sm-6 form-group">
+                                            <input
+                                                className="form-control"
+                                                type="password"
+                                                name="passOne"
+                                                placeholder="Password"
+                                                value={this.state.passOne}
+                                                onChange={this.changeFieldValue}
+                                            />
+                                        </section>
+                                        <section className="col-sm-6 form-group">
+                                            <input
+                                                className="form-control"
+                                                type="password"
+                                                required
+                                                name="passTwo"
+                                                placeholder="Repeat Password"
+                                                value={this.state.passTwo}
+                                                onChange={this.changeFieldValue}
+                                            />
+                                        </section>
+                                    </div>
+                                    <div className="form-group text-right mb-0">
+                                        <button className="btn btn-primary" type="submit">
+                                            Register
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </form>
         )
     }
 }
